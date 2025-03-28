16417be4e4d094eee6ebb24077393889db20ad95
Lucas <foxlucas05@icloud.com>
2025-03-28 13:44:44 -0500

    feat: user is able to enter a stock ticker with input validation

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ import yfinance as yf
 import json
 
 def main():
-    dat = yf.Ticker("AAPL")
+    while True:
+        ticker = input ("Enter a stock ticker: ").strip().upper()
+    
+        if not ticker.isalpha() or len(ticker) < 1:
+            print("Invalid ticker. Please enter a valid stock symbol \n") 
+        else: 
+            break
+
+    dat = yf.Ticker(ticker)
     info = dat.info
 
     print(json.dumps(info, indent=2))
