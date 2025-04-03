07584c51767da116e4365f2ea83edc1ea069ce25
Lucas <foxlucas05@icloud.com>
2025-04-03 11:05:56 -0500

    feat(backtest): run mock P/E strategy on energy sector and calculate 1-year return

--- backtests/pe_sector_adjusted.py
+++ backtests/pe_sector_adjusted.py
@@ -12,12 +12,16 @@ Strategy:
 
 NOTE: THIS IS NOT A VALID BACKTEST.
 This is simply for building skills in:
-- Filtering stocks based on current P/E
+- Filtering stocks based on fundamentals
 - Fetching historical price data
 - Calculating returns
 """
 
 def main():
+    """
+    Executes a mock P/E strategy to evaluate energy stocks in the S&P 500.
+    """
+
     pe_threshold = 16.71
 
     # --- Get Energy Tickers ---
@@ -30,11 +34,8 @@ def main():
     # Filter the DataFrame to include only rows where the sector is "Energy" and extract their ticker ("Symbol") as a list
     energy_tickers = sp500_df[sp500_df["GICS Sector"] == "Energy"]["Symbol"].tolist()
 
-
-    # --- Create a List of Stocks with P/E Below Threshold ---
+    # Loop through each ticker in energy_tickers and check the P/E value to create buy list
     buy_list = []
-
-    # Loop through each ticker in energy_tickers and check the P/E value
     for ticker in tqdm(energy_tickers):
         try:
             info = yf.Ticker(ticker).info
@@ -44,7 +45,45 @@ def main():
         except Exception as e:
             print(f"Error fetching data for {ticker}: {e}")
     
-    print(buy_list)
+    # Loop through each ticker in the buy list and get today's price and last year's price
+    price_dict = {}
+    for ticker in tqdm(buy_list):
+        try:
+            hist = yf.Ticker(ticker).history(period="1y") # Get DataFrame from past year
+            todays_price =  info.iloc[-1]["Close"]
+            last_years_price = info.iloc[0]["Close"]
+
+            price_dict[ticker] = {
+            "todays_price": float(todays_price),
+            "year_ago_price": float(last_years_price)
+            }
+        except Exception as e:
+            print(f"Error fetching data for {ticker}: {e}")
+
+    # --- Calculate return ---
+    returns = {}
+
+    for ticker, prices in price_dict.items():
+        today = prices["todays_price"]
+        year_ago = prices["year_ago_price"]
+        pct_return = (today - year_ago) / year_ago
+        returns[ticker] = pct_return
+        print(f"{ticker}: {pct_return:.2%}")
+
+    # Print backtest results
+    print("\n--- Backtest Summary ---")
+
+    total_return = sum(returns.values())
+    num_stocks = len(returns)
+    avg_return = total_return / num_stocks if num_stocks > 0 else 0
+    positive_returns = sum(1 for r in returns.values() if r > 0)
+    win_rate = (positive_returns / num_stocks) * 100 if num_stocks > 0 else 0
+
+    print(f"Number of Stocks: {num_stocks}")
+    print(f"Average Return:   {avg_return:.2%}")
+    print(f"Win Rate:         {win_rate:.2f}%")
+
+    print("\nScript completed.\n")
 
 
 if __name__ == "__main__":
