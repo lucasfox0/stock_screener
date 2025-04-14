9695730713536ab09ac82b8a2c53bfba57aec2dc
Lucas <foxlucas05@icloud.com>
2025-04-14 01:31:01 -0500

    feat: add proxies and tweak loop delay to cut run time down about 70 percent

--- scraping/data_collection.py
+++ scraping/data_collection.py
@@ -10,6 +10,8 @@ import json
 from termcolor import colored
 import datetime
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # --- Constants ---
 WIKI_URL = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
@@ -25,13 +27,15 @@ USER_AGENTS = [
     # Android Chrome
     "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.4896.127 Mobile Safari/537.36"
 ]
-DELAY_RANGE = (45, 90)  # seconds
+DELAY_RANGE = (15, 30)  # seconds
 ACCEPT_LANGUAGES = [
     "en-US,en;q=0.9",
     "en-GB,en;q=0.8",
     "en;q=0.7",
     "fr-FR,fr;q=0.9,en;q=0.8",
 ]
+SMARTPROXY_USERNAME = os.getenv("SMARTPROXY_USERNAME")
+SMARTPROXY_PASSWORD = os.getenv("SMARTPROXY_PASSWORD")
 
 
 def get_sector_tickers(sector):
@@ -62,13 +66,12 @@ def fetch_eps_data(ticker, url):
         headers = { # Human-like headers
             "User-Agent": random.choice(USER_AGENTS),
             "Accept-Language": random.choice(ACCEPT_LANGUAGES),
-            "Accept-Encoding": "gzip, deflate, br",
-            "Connection": "keep-alive",
-            "Referer": "https://www.google.com/",
-            "DNT": "1",  # Do Not Track
-            "Upgrade-Insecure-Requests": "1"
         }
-        response = requests.get(url, headers=headers)
+        proxy = {
+            "http": f"http://{SMARTPROXY_USERNAME}:{SMARTPROXY_PASSWORD}@gate.smartproxy.com:10001",
+            "https": f"http://{SMARTPROXY_USERNAME}:{SMARTPROXY_PASSWORD}@gate.smartproxy.com:10001"
+        }
+        response = requests.get(url, headers=headers, proxies=proxy, timeout=30)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, "html.parser")
         tables = soup.find_all("table")
@@ -111,11 +114,13 @@ def scrape_ticker_eps(ticker, max_attempts=5):
                 }
             }, None
         
+
+        # Apply a delay that increases exponetially with each attempt
+        delay = min(random.uniform(*DELAY_RANGE) * (1.7 ** attempts), 300)
+        print(colored(f"{attempts + 1} try for {ticker} - sleeping {delay:.0f} seconds...", "yellow"))
+
         attempts += 1
 
-        # Apply a delay that increases with each attempt
-        delay = random.uniform(*DELAY_RANGE) + attempts * 30
-        print(colored(f"{attempts} try for {ticker} - sleeping {delay:.0f} seconds...", "yellow"))
         time.sleep(delay)
         
     return None, f"Failed to fetch EPS for {ticker} after {max_attempts} attempts"
@@ -130,7 +135,7 @@ def collect_and_save_eps_data(output_path):
     all_eps_data = {}
     failed_tickers_log = {}
 
-    for ticker in tqdm(energy_tickers[:5], desc="Scraping EPS"):
+    for ticker in tqdm(energy_tickers, desc="Scraping EPS"):
         print(f"\nStarting {ticker}...")
         data, error = scrape_ticker_eps(ticker)
 
