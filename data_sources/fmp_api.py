3bd42e5b2ea551774e324f9736680fbeacfc91e0
Lucas <foxlucas05@icloud.com>
2025-04-17 13:18:27 -0500

    feat: obtain a list of all energy tickers with the following criteria:
    - Greater than 10B market cap
    - Actively trading
    - NYSE
    - Is not an ETF or Fund
    - In the Energy sector

--- data_sources/fmp_api.py
+++ data_sources/fmp_api.py
@@ -1,12 +1,35 @@
-from config import FMP_KEY
 import requests
+import json
+from config import FMP_KEY, BASE_URL
+
+def get_energy_tickers():
+    """Return a list of energy tickers"""
+    endpoint = f"{BASE_URL}/v3/stock-screener"
+    params = {
+        "isActivelyTrading": True,
+        "exchange": "nyse",
+        "marketCapMoreThan": 10_000_000_000, # 10B (Large Cap)
+        "isEtf": "false",
+        "isFund": "false",
+        "sector": "Energy",
+        "apikey": FMP_KEY
+    }
+
+    try:
+        response = requests.get(endpoint, params=params)
+        response.raise_for_status()
+        data = response.json()
+    except Exception as e:
+        return None, f"Error fetching data: {e}"
+    
+    # Extract ticker symbols from each dictionary in the response
+    energy_tickers = [item["symbol"] for item in data]
+
+    return energy_tickers, None
+
 
 def main():
-    ticker = "AAPL"
-    url = f"https://financialmodelingprep.com/stable/search-symbol?query={ticker}&apikey={FMP_KEY}"
-    response = requests.get(url)
-    data = response.json()
-    print(data)
+    energy_tickers, error = get_energy_tickers()
 
 if __name__ == "__main__":
     main()
