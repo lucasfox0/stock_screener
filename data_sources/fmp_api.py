967d81a582e878aceb47a571bc6eaf10892dff1e
Lucas <foxlucas05@icloud.com>
2025-04-17 15:08:52 -0500

    feat: fetch and save quarterly pe data to JSON

--- data_sources/fmp_api.py
+++ data_sources/fmp_api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 from config import FMP_KEY, BASE_URL
 
 def get_tickers(sector):
@@ -27,9 +28,51 @@ def get_tickers(sector):
 
     return tickers, None
 
+def get_historical_pe(tickers, output_path):
+    """Save historical P/E data for a list of tickers to a JSON"""
+    params = {
+        "period": "quarter",
+        "limit": 40,
+        "apikey": FMP_KEY
+    }
+
+    historical_pe = {} # Main dictionary to store all P/E data
+    for ticker in tickers:
+        # Build endpoint URL using the ticker and set limit=40 (10 years of quarterly data)
+        endpoint = f"{BASE_URL}/v3/ratios/{ticker}"
+        
+        # Make GET request to fetch data and parse the JSON response
+        try:
+            response = requests.get(endpoint, params=params)
+            response.raise_for_status()
+            data = response.json()
+        except Exception as e:
+            return None, f"Error fetching data: {e}"
+
+        # Extract the "priceEarningsRatio" from each quarterly entry
+        for quarter in data:
+            date = quarter["date"]
+            pe = quarter["priceEarningsRatio"]
+
+            if ticker not in historical_pe:
+                historical_pe[ticker] = {}
+            
+            historical_pe[ticker][date] = pe
+
+    # Save the historical_pe dict to a JSON
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    with open(output_path, "w") as f:
+        json.dump(historical_pe, f, indent=2)
+
+    print(f"\nP/E data saved to {output_path}")
+
 
 def main():
     energy_tickers, error = get_tickers("Energy")
+
+    if energy_tickers:
+        get_historical_pe(energy_tickers, "../data/energy_pe.json")
 
 if __name__ == "__main__":
     main()
