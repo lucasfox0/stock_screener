afa2234ec7e2692636e6f5a1897492f88670cc73
Lucas <foxlucas05@icloud.com>
2025-04-18 10:41:04 -0500

    refactor: delete deprecated function

--- data_sources/fmp_api.py
+++ data_sources/fmp_api.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 from config import FMP_KEY, BASE_URL
 
+
 def get_tickers(sector):
     """Return a list of energy tickers"""
     endpoint = f"{BASE_URL}/v3/stock-screener"
@@ -29,45 +30,6 @@ def get_tickers(sector):
 
     return tickers, None
 
-# Old method for fetching historical P/E. Now used since P/E is being calculated manually
-def deprecated_get_historical_pe(tickers, output_path):
-    """Save historical P/E data for a list of tickers to a JSON"""
-    params = {
-        "period": "quarter",
-        "limit": 40,
-        "apikey": FMP_KEY
-    }
-
-    historical_pe = {} # Main dictionary to store all P/E data
-    for ticker in tickers:
-        # Build endpoint URL using the ticker and set limit=40 (10 years of quarterly data)
-        endpoint = f"{BASE_URL}/v3/ratios/{ticker}"
-        
-        # Make GET request to fetch data and parse the JSON response
-        try:
-            response = requests.get(endpoint, params=params)
-            response.raise_for_status()
-            data = response.json()
-        except Exception as e:
-            return None, f"Error fetching data: {e}"
-
-        # Extract the "priceEarningsRatio" from each quarterly entry
-        for quarter in data:
-            date = quarter["date"]
-            pe = quarter["priceEarningsRatio"]
-
-            if ticker not in historical_pe:
-                historical_pe[ticker] = {}
-            
-            historical_pe[ticker][date] = pe
-
-    # Save the historical_pe dict to a JSON
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-    with open(output_path, "w") as f:
-        json.dump(historical_pe, f, indent=2)
-
-    print(f"\nP/E data saved to {output_path}")
 
 def get_monthly_closing_price(ticker, output_path):
     """Get the monthly closing price of a ticker"""
@@ -103,9 +65,10 @@ def get_monthly_closing_price(ticker, output_path):
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
     with open(output_path, "w") as f:
-        json.dump(monthly_close_dict, f, indent=4)
+        json.dump(monthly_close_dict, f, indent=2)
 
     print(f"Montly close data saved to {output_path}")
+
 
 def main():
     monthly_close = get_monthly_closing_price("XOM", "../data/monthly_close.json")
